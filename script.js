65f0dad67912d6dc0841dfb8d38030cb75222bf5
Vinicius Dill Da Luz <vinidill9@gmail.com>
2024-02-20 13:26:08 -0300

    feat: add close toggle

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-let show = true
+let show = true;
 
-const menuContent = document.querySelector(".content")
-const menuToggle = menuContent.querySelector(".menu-toggle")
-
+const menuContent = document.querySelector(".content");
+const menuToggle = menuContent.querySelector(".menu-toggle");
+const menuLinks = menuContent.querySelectorAll(".list-menu li a");
 
 menuToggle.addEventListener("click", () => {
-    menuContent.classList.toggle("on", show)
-    show =!show
-})
+    menuContent.classList.toggle("on", show);
+    show = !show;
+});
+
+menuLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        menuContent.classList.remove("on");
+    });
+});
