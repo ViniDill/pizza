b69d24d3ceec7cd4cdab25586450f0a017a246f7
Vinicius Dill Da Luz <vinidill9@gmail.com>
2024-02-20 13:30:07 -0300

    feat: add funcionality

--- script.js
+++ script.js
@@ -11,6 +11,16 @@ menuToggle.addEventListener("click", () => {
 
 menuLinks.forEach(link => {
     link.addEventListener("click", () => {
+
         menuContent.classList.remove("on");
+
+        document.querySelectorAll("section").forEach(section => {
+            section.classList.remove("section-spacing");
+        });
+
+        const targetId = link.getAttribute("href").substring(1);
+        const targetSection = document.getElementById(targetId);
+
+        targetSection.classList.add("section-spacing");
     });
 });
